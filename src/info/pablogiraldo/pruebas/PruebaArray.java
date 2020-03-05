2ba98d1bfb4e0ec864b19c68299f959b9a06712b
laborus <saulogiral@gmail.com>
2020-03-05 13:10:51 +0100

    Update

--- src/info/pablogiraldo/pruebas/PruebaArray.java
+++ src/info/pablogiraldo/pruebas/PruebaArray.java
@@ -11,16 +11,27 @@ public class PruebaArray {
 		numeros.add(5);
 		numeros.add(7);
 		System.out.println("\n");
+		System.out.println("Contenido números:");
 		for (int num : numeros) {
 			System.out.println(num);
 		}
+		boolean contiene = numeros.contains(7);
+
+		System.out.println("\n");
+		System.out.println("Contiene 7: " + contiene);
 
 		numeros.removeAll(Arrays.asList(7));
 
+		contiene = numeros.contains(7);
+
 		System.out.println("\n");
+		System.out.println("Contenido números:");
 		for (int num : numeros) {
 			System.out.println(num);
 		}
+
+		System.out.println("\n");
+		System.out.println("Contiene 7: " + contiene);
 	}
 
 }
